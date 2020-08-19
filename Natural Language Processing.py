019949d88e32bbac0c69028a00db74ccfdec3f72
Ganti Sharan <63344473+ganti-sharan@users.noreply.github.com>
2020-08-19 16:35:46 +0530

    Add files via upload

---
+++ Natural Language Processing.py
@@ -0,0 +1,55 @@
+# Natural Language Processing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing Dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+# Cleaning the texts
+import re 
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0,1000) :
+    review = re.sub('[^a-zA-Z]',' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+#Creating bag of words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features =1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:,1].values
+
+# Test train split
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test = train_test_split(X,y,test_size=0.25,random_state=0)
+
+#Fitting classifier
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 800,
+                                    criterion = 'entropy')
+classifier.fit(X_train,Y_train)
+
+#predicting test set result
+Y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test,Y_pred)
+cm
+obj = ['nice place and great food']
+obj1 = cv.transform(obj).toarray()
+classifier.predict(obj1)
+(103+70)/250
+# NTUSD-SIN
+# NTUSD
